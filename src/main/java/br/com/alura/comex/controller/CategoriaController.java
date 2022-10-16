29e32f6337cd95922acbc89fc459df31f69b7573
thpandrade <thiago.andrade@loft.com.br>
2022-10-16 17:14:06 -0300

    feat: add database config

--- src/main/java/br/com/alura/comex/controller/CategoriaController.java
+++ src/main/java/br/com/alura/comex/controller/CategoriaController.java
@@ -2,8 +2,10 @@ package br.com.alura.comex.controller;
 
 import br.com.alura.comex.controller.dto.NovaCategoriaRequest;
 import br.com.alura.comex.repository.CategoriaRepository;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -19,8 +21,8 @@ class CategoriaController {
     }
 
 
-    @PostMapping()
-    ResponseEntity<Void> novaCategoria(NovaCategoriaRequest novaCategoriaRequest, UriComponentsBuilder uriBuilder) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<Void> novaCategoria(@RequestBody NovaCategoriaRequest novaCategoriaRequest, UriComponentsBuilder uriBuilder) {
 
         var novaCategoria = novaCategoriaRequest.toCategoriaAtiva();
 
