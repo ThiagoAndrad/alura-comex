ccc9c5555fa8231d42967621414a5ecd4134d316
thpandrade <thiago.andrade@loft.com.br>
2022-10-29 19:28:11 -0300

    feat: add http status to http exceptions

--- src/main/java/br/com/alura/comex/exception/UnprocessableEntityException.java
+++ src/main/java/br/com/alura/comex/exception/UnprocessableEntityException.java
@@ -1,5 +1,9 @@
 package br.com.alura.comex.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
 public class UnprocessableEntityException extends RuntimeException {
     public UnprocessableEntityException(String message) {
         super(message);
